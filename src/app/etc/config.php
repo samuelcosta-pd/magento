16aedd376af31c31cac9bfe233af0285195900ff
samuelcosta-pd <samuelcosta.pd@gmail.com>
2026-09-09 09:28:29 -0300

    feat: catalog module implementation

--- src/app/etc/config.php
+++ src/app/etc/config.php
@@ -384,6 +384,7 @@ return [
         'PayPal_BraintreeGiftWrapping' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'PayPal_BraintreeReward' => 1,
+        'Webjump_CatalogBehavior' => 1,
         'Webjump_HelloWorld' => 1
     ]
 ];
