391eeb84c887956d1696a691444685f60eb2974a
samuelcosta-pd <samuelcosta.pd@gmail.com>
2026-09-16 08:23:39 -0300

    feat: scaffold module, declarative schema and whitelist

--- src/app/etc/config.php
+++ src/app/etc/config.php
@@ -384,7 +384,8 @@ return [
         'PayPal_BraintreeGiftWrapping' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'PayPal_BraintreeReward' => 1,
-        'Webjump_HelloWorld' => 0,
+        'Webjump_CatalogBehavior' => 1,
+        'Webjump_ProductReview' => 1,
         'Webjump_Samuel' => 1
     ]
 ];
