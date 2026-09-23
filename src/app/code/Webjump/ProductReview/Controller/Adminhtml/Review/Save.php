b56e78299054055fcc12dd0bc25f47dd6867a743
samuelcosta-pd <samuelcosta.pd@gmail.com>
2026-09-23 11:27:12 -0300

    fix(ProductReview): add canRestore attribute to system config fields and format PDP review template

--- src/app/code/Webjump/ProductReview/Controller/Adminhtml/Review/Save.php
+++ src/app/code/Webjump/ProductReview/Controller/Adminhtml/Review/Save.php
@@ -64,7 +64,7 @@ class Save extends Action implements HttpPostActionInterface
                 try {
                     $review = $this->reviewRepository->getById($id);
                 } catch (NoSuchEntityException $e) {
-                    throw new LocalizedException(__('Esta avaliação não existe mais.'));
+                    throw new LocalizedException(__('Esta avaliação não existe mais para edição.'));
                 }
             } else {
                 $review = $this->reviewFactory->create();
